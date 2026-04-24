a5f79cc56cfbf0a517492caf65b0c9ebcaf4d901
Qimochi <81931118+c0desk1@users.noreply.github.com>
2026-04-24 17:24:00 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
+import { siteConfig } from './src/config/site.ts';
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
-
 import partytown from '@astrojs/partytown';
-
 import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://c0desk1.my.id/",
+  site: siteConfig.url,
   trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()]
