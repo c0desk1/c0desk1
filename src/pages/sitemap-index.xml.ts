79c9341cd5037418e7847392d6c7e510ef89733a
Qimochi <81931118+c0desk1@users.noreply.github.com>
2026-04-26 18:41:38 +0700

    Update sitemap-index.xml.ts

--- src/pages/sitemap-index.xml.ts
+++ src/pages/sitemap-index.xml.ts
@@ -7,19 +7,25 @@ const siteUrl = siteConfig.url;
 export const GET: APIRoute = () => {
   const sitemaps = [
     'sitemap-pages.xml',
-    'sitemap-author.xml',
     'sitemap-blog.xml',
     'sitemap-portfolio.xml',
+    'sitemap-author.xml',
   ];
+
+  const buildDate = new Date().toISOString();
 
   const sitemapIndex = `<?xml version="1.0" encoding="UTF-8"?>
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 ${sitemaps.map(sitemap => `  <sitemap>
     <loc>${new URL(sitemap, siteUrl).toString()}</loc>
+    <lastmod>${buildDate}</lastmod>
   </sitemap>`).join('\n')}
 </sitemapindex>`;
 
   return new Response(sitemapIndex, {
-    headers: { 'Content-Type': 'application/xml' },
+    headers: { 
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=0, must-revalidate'
+    },
   });
 };
