232245e336f5b9dce66e402a2d01b608bb8cacaf
Qimochi <81931118+c0desk1@users.noreply.github.com>
2026-04-26 18:40:37 +0700

    Update sitemap-pages.xml.ts

--- src/pages/sitemap-pages.xml.ts
+++ src/pages/sitemap-pages.xml.ts
@@ -7,19 +7,23 @@ const siteUrl = siteConfig.url;
 const pages = [
   { url: '' },
   { url: 'about/' },
-  { url: 'blog/' },
-  { url: 'portfolio/' },
 ];
 
 export const GET: APIRoute = () => {
+  const buildDate = new Date().toISOString();
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-${pages.map(page => ` <url>
+${pages.map(page => `  <url>
     <loc>${new URL(page.url, siteUrl).toString()}</loc>
+    <lastmod>${buildDate}</lastmod>
   </url>`).join('\n')}
 </urlset>`;
 
   return new Response(sitemap, {
-    headers: { 'Content-Type': 'application/xml' },
+    headers: { 
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=0, must-revalidate'
+    },
   });
 };
