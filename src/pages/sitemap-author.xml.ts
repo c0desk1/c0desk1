d68198530f27e97aeebaf788078d661257250932
Qimochi <81931118+c0desk1@users.noreply.github.com>
2026-04-26 18:38:43 +0700

    Update sitemap-author.xml.ts

--- src/pages/sitemap-author.xml.ts
+++ src/pages/sitemap-author.xml.ts
@@ -6,21 +6,28 @@ import { siteConfig } from '@/config/site';
 const siteUrl = siteConfig.url;
 
 export const GET: APIRoute = async () => {
-  const posts = await getCollection('blog', ({ data }) =>!data.draft);
+  const posts = await getCollection('blog', ({ data }) => !data.draft);
 
   const latestPost = posts.sort((a, b) =>
     b.data.pubDate.getTime() - a.data.pubDate.getTime()
   )[0];
 
+  const authorLastMod = latestPost
+    ? (latestPost.data.updatedDate || latestPost.data.pubDate).toISOString()
+    : new Date().toISOString();
+
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>${new URL(`author/${siteConfig.author.slug}/`, siteUrl).toString()}</loc>
-    <lastmod>${latestPost?.data.updatedDate?.toISOString() || latestPost?.data.pubDate.toISOString()}</lastmod>
+    <lastmod>${authorLastMod}</lastmod>
   </url>
 </urlset>`;
 
   return new Response(sitemap, {
-    headers: { 'Content-Type': 'application/xml' },
+    headers: { 
+      'Content-Type': 'application/xml',
+      'Cache-Control': 'public, max-age=0, must-revalidate'
+    },
   });
 };
