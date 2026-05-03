1368c5f00eedd20da979006e6a0adbe7b4af73f4
c0desk1 <dev.c0desk1@gmail.com>
2026-05-03 21:51:45 +0700

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,5 +1,11 @@
 /// <reference types="astro/client" />
 
+interface Window {
+  toast: {
+    show: (message: string, options?: { duration?: number; type?: 'success' | 'error' | 'info' }) => void;
+  };
+}
+
 declare namespace JSX {
   interface IntrinsicElements {
     [elemName: string]: any;
