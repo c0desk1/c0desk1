e35733516f64a0bd43198e4597582940d004b034
c0desk1 <dev.c0desk1@gmail.com>
2026-05-05 03:30:51 +0700

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,19 @@ import { siteConfig } from './src/config/site.ts';
 import tailwindcss from '@tailwindcss/vite';
 import mdx from '@astrojs/mdx';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
   site: siteConfig.siteUrl,
   trailingSlash: "always",
   output: 'static',
   i18n: {
-	defaultLocale: "id",
-	locales: ["id", "en"],
-	routing: {
-		prefixDefaultLocale: false
-	}
+    defaultLocale: "id",
+    locales: ["id", "en"],
+    routing: {
+        prefixDefaultLocale: false
+    }
   },
   vite: {
     plugins: [tailwindcss()]
@@ -22,9 +24,7 @@ export default defineConfig({
   build: {
     inlineStylesheets: 'auto'
   },
-  integrations: [
-    mdx(),
-  ],
+  integrations: [mdx(), react()],
   image: {
     remotePatterns: [
       { protocol: 'https', hostname: 'cdn.c0desk1.my.id' }
