e63d7eaf0395b4563d29ef7da51941bafacc952f
c0desk1 <dev.c0desk1@gmail.com>
2026-05-05 22:35:33 +0700

    Update MDXComponents.tsx

--- src/components/layout/MDXComponents.tsx
+++ src/components/layout/MDXComponents.tsx
@@ -1,8 +1,8 @@
 // src/components/layout/MDXComponents.tsx
 
-import Heading from '@/components/mdx/HeadingAnchor.astro';
 import type { MDXComponents } from 'mdx/types';
 
+import Heading from '@/components/mdx/Heading.astro';
 import Image from "@/components/mdx/Image.astro";
 import HR from "@/components/mdx/HR.astro";
 import Table from '@/components/mdx/Table.astro';
@@ -14,12 +14,8 @@ import InlineLink from '@/components/mdx/InlineLink.astro';
 import CallQuote from '@/components/mdx/CallQuote.astro';
 
 export const components: MDXComponents = {
-  h1: Heading,
   h2: Heading,
   h3: Heading,
-  h4: Heading,
-  h5: Heading,
-  h6: Heading,
   a: InlineLink,
   img: Image,
   hr: HR,
