57dab48c03c570e834b1b9d47cb017fcc658696a
c0desk1 <dev.c0desk1@gmail.com>
2026-05-08 01:27:03 +0700

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,5 +1,6 @@
-import { defineCollection, reference, z } from "astro:content";
+import { defineCollection, reference } from "astro:content";
 import { glob } from "astro/loaders";
+import { z } from 'astro/zod';
 
 // ==========================================
 // 1. SHARED UTILITIES
