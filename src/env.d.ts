5c6093583c020b27296ea9b6d84f2efda3309833
c0desk1 <dev.c0desk1@gmail.com>
2026-05-08 00:31:33 +0700

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,12 @@
 /// <reference types="astro/client" />
 
 interface Window {
+  __YANDEX_FLOOR_LOADED__?: boolean;
+  __YANDEX_IMAGE_LOADED__?: boolean;
+  yaContextCb: Array<() => void>;
+}
+
+interface Window {
   toast: {
     show: (message: string, options?: { duration?: number; type?: 'success' | 'error' | 'info' }) => void;
   };
