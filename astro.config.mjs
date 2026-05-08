f554c8617410163a9b07628aebd4153687c04368
c0desk1 <dev.c0desk1@gmail.com>
2026-05-08 19:23:12 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,7 +11,7 @@ import cloudflare from '@astrojs/cloudflare';
 export default defineConfig({
   site: siteConfig.siteUrl,
   trailingSlash: "always",
-  output: 'static',
+  output: 'server',
   adapter: cloudflare(),
   i18n: {
     defaultLocale: "id",
