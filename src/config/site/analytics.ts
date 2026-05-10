ee5edf0b1cbcc57352f5710c7b18c89d94c2a2f0
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-05-11 02:37:52 +0700

    Create analytics.ts

---
+++ src/config/site/analytics.ts
@@ -0,0 +1,13 @@
+export const analyticsConfig = {
+  googleSiteVerification: "ZxoHYWipAMoGhvwVi5nxolWsZOntrYkNkVcG9HTfFZQ",
+  googleAnalyticsId: "G-2TKFP229HJ",
+  googleAdsenseId: "ca-pub-4943136052113535",
+  googleAdsenseEnabled: false,
+
+  yandexVerification: "1878480c0d7b5510",
+  yandexMetricaId: "108731154",
+  yandexPubId: "R-A-19179231",
+  yandexAdsEnabled: true,
+
+  adPriority: "yandex" as "yandex" | "adsense" | "both",
+} as const;
