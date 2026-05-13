624e74e758b5b1c72a94d9349caa8d3cc8eeac5e
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-05-13 15:45:45 +0700

    Update analytics.ts

--- src/config/site/analytics.ts
+++ src/config/site/analytics.ts
@@ -2,7 +2,7 @@ export const analyticsConfig = {
   googleSiteVerification: "ZxoHYWipAMoGhvwVi5nxolWsZOntrYkNkVcG9HTfFZQ",
   googleAnalyticsId: "G-2TKFP229HJ",
   googleAdsenseId: "ca-pub-4943136052113535",
-  googleAdsenseEnabled: false,
+  googleAdsenseEnabled: true,
 
   yandexVerification: "1878480c0d7b5510",
   yandexMetricaId: "108731154",
