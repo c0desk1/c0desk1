22ed9f70fad77c37dcb3c9efca3a8e47b399e85a
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-05-18 02:59:18 +0700

    Add files via upload

---
+++ src/components/layout/MDXComponents.tsx
@@ -0,0 +1,43 @@
+// src/components/layout/MDXComponents.tsx
+
+import type { MDXComponents } from 'mdx/types';
+
+import Heading from '@/components/mdx/HeadingAnchor.astro';
+import P from '@/components/mdx/P.astro';
+import LI from '@/components/mdx/LI.astro';
+import UL from '@/components/mdx/UL.astro';
+import OL from '@/components/mdx/OL.astro';
+import Image from "@/components/mdx/imageMDX.astro";
+import HR from "@/components/mdx/HR.astro";
+import Table from '@/components/mdx/Table.astro';
+import Details from '@/components/mdx/Details.astro';
+import Steps from '@/components/mdx/Steps.astro';
+import Code from '@/components/mdx/Code.astro';
+import InlineLink from '@/components/mdx/InlineLink.astro';
+import Blockquote from '@/components/mdx/Blockquote.astro';
+import Footnotes from '@/components/mdx/Footnotes.astro';
+import Callout from '../mdx/Callout.astro';
+
+export const components: MDXComponents = {
+  h1: Heading,
+  h2: Heading,
+  h3: Heading,
+  h4: Heading,
+  h5: Heading,
+  h6: Heading,
+  p: P,
+  li: LI,
+  ul: UL,
+  ol: OL,
+  a: InlineLink,
+  img: Image,
+  hr: HR,
+  blockquote: Blockquote,
+  Blockquote,
+  table: Table,
+  Details,
+  Steps,
+  Code,
+  Footnotes,
+  Callout,
+};
