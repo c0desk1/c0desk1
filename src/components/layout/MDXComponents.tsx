71e74bfaeb098f2d2f6948c2199cd1520532dd7a
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-05-26 21:53:26 +0700

    Add files via upload

---
+++ src/components/layout/MDXComponents.tsx
@@ -0,0 +1,58 @@
+// src/components/layout/MDXComponents.tsx
+
+import type { MDXComponents } from 'mdx/types';
+
+import Heading from '@/components/mdx/Heading.astro';
+import P from '@/components/mdx/P.astro';
+import LI from '@/components/mdx/LI.astro';
+import UL from '@/components/mdx/UL.astro';
+import OL from '@/components/mdx/OL.astro';
+import Image from "@/components/mdx/imageMDX.astro";
+import HR from "@/components/mdx/HR.astro";
+import Table from '@/components/mdx/Table.astro';
+import Details from '@/components/mdx/Details.astro';
+import Steps from '@/components/mdx/Steps.astro';
+import Code from '@/components/mdx/Code.astro';
+import InlineLink from '@/components/mdx/InlineLink.astro';
+import Blockquote from '@/components/mdx/Blockquote.astro';
+import Footnotes from '@/components/mdx/Footnotes.astro';
+import Callout from '../mdx/Callout.astro';
+import Accordion from '../mdx/Accordion.astro';
+import Timeline from '../mdx/Timeline.astro';
+import Badge from '../mdx/Badge.astro';
+import KBD from '../mdx/KBD.astro';
+import Changelog from '../mdx/changelog.astro';
+import Banner from '../mdx/Banner.astro';
+import Video from '../mdx/Video.astro';
+
+export const components: MDXComponents = {
+  h1: Heading,
+  h2: Heading,
+  h3: Heading,
+  h4: Heading,
+  h5: Heading,
+  h6: Heading,
+  p: P,
+  li: LI,
+  ul: UL,
+  ol: OL,
+  a: InlineLink,
+  img: Image,
+  hr: HR,
+  Blockquote,
+  table: Table,
+  Details,
+  Steps,
+  Timeline,
+  Code,
+  footnotes: Footnotes,
+  Footnotes,
+  Changelog,
+  Callout,
+  Accordion,
+  Badge,
+  kbd: KBD,
+  KBD,
+  Banner,
+  Video
+};
