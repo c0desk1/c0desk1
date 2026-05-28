8910f5d87bdcffbb224af5b8a7475f1e4ede531f
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-05-28 15:52:13 +0700

    Update feed.xml.ts

--- src/pages/feed.xml.ts
+++ src/pages/feed.xml.ts
@@ -1,7 +1,12 @@
 // src/pages/feed.xml.ts
 import type { APIRoute } from "astro";
 import { getCollection, getEntry } from "astro:content";
-import { SITE, AUTHOR, PAGINATION, CATEGORIES } from "@/consts";
+import { 
+  SITE, 
+  AUTHOR, 
+  CATEGORIES,
+  PAGINATION 
+} from "@/consts";
 import { slugify } from "@/lib/utils";
 
 export const GET: APIRoute = async () => {
@@ -11,45 +16,58 @@ export const GET: APIRoute = async () => {
     .sort((a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf())
     .slice(0, PAGINATION.postsPerFeed);
 
-  const items = await Promise.all(
-    sorted.map(async (post) => {
-      const slug = post.data.slug || slugify(post.data.title);
-      const url = `${SITE.url}/blog/${slug}`;
+  const items: string[] = [];
 
-      let authorName = AUTHOR.name;
-      if (post.data.author) {
-        if (typeof post.data.author === "string" && /^[a-z0-9-]+$/.test(post.data.author)) {
-          try {
-            const authorEntry = await getEntry("authors", post.data.author);
-            if (authorEntry?.data?.name) {
-              authorName = authorEntry.data.name;
-            }
-          } catch {}
-        } else if (typeof post.data.author === "object" && (post.data.author as any)?.name) {
-          authorName = (post.data.author as any).name;
+  for (const post of sorted) {
+    const slug = post.data.slug || slugify(post.data.title);
+    const url = `${SITE.url}/blog/${slug}`;
+
+    let authorName = `${SITE.email} (${AUTHOR.name})`;
+    if (post.data.author) {
+      try {
+        const authorEntry = await getEntry(post.data.author);
+        
+        if (authorEntry?.data?.name) {
+          const authorEmail = authorEntry.data.mail || SITE.email;
+          authorName = `${authorEmail} (${authorEntry.data.name})`;
         }
+      } catch {}
+    }
+
+    const pubDate = post.data.pubDate.toUTCString();
+
+    const imageSrc =
+      post.data.heroImage?.src ||
+      post.data.seo?.ogImage ||
+      SITE.ogImage;
+
+    const coverImg = imageSrc
+      ? `<enclosure url="${new URL(imageSrc, SITE.url).href}" type="image/jpeg" length="0"/>`
+      : "";
+
+    let categoryName = "General";
+    const rawCategory = post.data.category;
+
+    if (rawCategory) {
+      if (typeof rawCategory === "object" && "collection" in rawCategory) {
+        try {
+          const catEntry = await getEntry(rawCategory);
+          const slugToFind = catEntry?.data?.slug || catEntry?.id;
+          const matched = CATEGORIES.find(c => c.slug === slugToFind);
+          categoryName = matched ? matched.label : (slugToFind || "General");
+        } catch {}
+      } else {
+        const categorySlug = String(rawCategory);
+        const matched = CATEGORIES.find(c => c.slug === categorySlug);
+        
+        categoryName = matched 
+          ? matched.label 
+          : categorySlug.charAt(0).toUpperCase() + categorySlug.slice(1);
       }
+    }
 
-      const pubDate = post.data.pubDate.toUTCString();
 
-      const imageSrc =
-        post.data.heroImage?.src ||
-        post.data.seo?.ogImage ||
-        SITE.ogImage;
-
-      const coverImg = imageSrc
-        ? `<enclosure url="${new URL(imageSrc, SITE.url).href}" type="image/jpeg" length="0"/>`
-        : "";
-
-      let categoryName = "Uncategorized";
-      const rawCat = post.data.category as any;
-      if (rawCat) {
-        const catSlug = typeof rawCat === "string" ? rawCat : rawCat.slug || rawCat.id || "";
-        const matched = CATEGORIES.find((c) => c.slug === catSlug);
-        categoryName = matched?.label || catSlug || "Uncategorized";
-      }
-
-      return `
+    const itemXml = `
     <item>
       <title><![CDATA[${post.data.title}]]></title>
       <link>${url}</link>
@@ -65,27 +83,28 @@ export const GET: APIRoute = async () => {
       }
       ${coverImg}
     </item>`;
-    })
-  );
+    
+    items.push(itemXml);
+  }
 
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
 <rss version="2.0"
-  xmlns:content="http://purl.org/rss/1.0/modules/content/"
-  xmlns:atom="http://www.w3.org/2005/Atom"
-  xmlns:media="http://search.yahoo.com/mrss/">
+  xmlns:content="http://purl.org"
+  xmlns:atom="http://w3.org"
+  xmlns:media="http://yahoo.com">
   <channel>
     <title>${SITE.name}</title>
     <link>${SITE.url}</link>
     <description>${SITE.description}</description>
     <language>${SITE.lang}</language>
     <copyright>© ${new Date().getFullYear()} ${SITE.name}</copyright>
-    <managingEditor>${SITE.name} (${SITE.email})</managingEditor>
+    <managingEditor>${SITE.email} (${SITE.name})</managingEditor>
     <webMaster>${SITE.email}</webMaster>
     <lastBuildDate>${new Date().toUTCString()}</lastBuildDate>
     <ttl>60</ttl>
     <atom:link href="${SITE.url}/feed.xml" rel="self" type="application/rss+xml"/>
     <image>
-      <url>${SITE.url}/favicon-96x96.png</url>
+      <url>${SITE.url}/favicon/favicon-96x96.png</url>
       <title>${SITE.name}</title>
       <link>${SITE.url}</link>
     </image>
