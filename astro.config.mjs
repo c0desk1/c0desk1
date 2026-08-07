f4ffc6fe43bb80ac442010903843a93f17c6da0c
c0desk1 <dev.c0desk1@gmail.com>
2026-08-08 00:41:43 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import {
 
 import { SITE } from "./src/consts";
 
-
 import { 
   satteri, 
   satteriHeadingIdsPlugin 
@@ -28,21 +27,18 @@ import {
 
 import { satteriHeadingAnchor } from "./src/lib/mdx/satteri-heading-anchor";
 import { satteriExternalLink } from "./src/lib/mdx/satteri-external-link";
-
+import { satteriTabsHast } from "./src/lib/mdx/satteri-tab-hast";
 
 import { satteriCallout } from "./src/lib/mdx/satteri-callout";
 import { satteriFileTree } from "./src/lib/mdx/satteri-filetree";
 import { satteriSteps } from "./src/lib/mdx/satteri-steps";
 import { satteriTabs } from "./src/lib/mdx/satteri-tabs";
 import { satteriAccordion } from "./src/lib/mdx/satteri-accordion";
-
 import { satteriQuote } from "./src/lib/mdx/satteri-blockquote";
 import { satteriChangelog } from "./src/lib/mdx/satteri-changelog";
 import { satteriCodeBlock } from "./src/lib/mdx/satteri-code-block";
 import { satteriFigure } from "./src/lib/mdx/satteri-figure";
 import { satteriKbd } from "./src/lib/mdx/satteri-kbd";
-
-import { satteriTabsHast } from "./src/lib/mdx/satteri-tab-hast";
 
 import pagefind from "astro-pagefind";
 
